339586571497fb7b15d2d5456b441ed7486ca864
1SMOKE1 <kamyshan19@gmail.com>
2023-07-16 14:21:12 +0300

    add code for images preview

--- src/modules/products/controllers/products.controller.ts
+++ src/modules/products/controllers/products.controller.ts
@@ -65,9 +65,9 @@ export class ProductsController {
       const path = decodeURIComponent(JSON.parse(query));
       const file = fs.createReadStream(join(`${process.cwd()}/uploads/images`, path.split('\\')[2]));
 
-      res.setHeader('Content-Type', 'application/octet-stream');
-      res.setHeader('Content-Disposition', `attachment; filename="${path.split('-')[2]}"`)
-
+      // res.setHeader('Content-Type', 'application/octet-stream');
+      // res.setHeader('Content-Disposition', `attachment; filename="${path.split('-')[2]}"`)
+      res.type('application/octet-stream');
       file.pipe(res);
     } catch (err) {
       throw new BadRequestException(err)
