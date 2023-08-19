f34e3e51aeaa35c5bf70dcbba1eadbc86cf42e1f
1SMOKE1 <kamyshan19@gmail.com>
2023-08-19 15:21:35 +0300

    add cache to messages

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -3,10 +3,12 @@ import { OrdersController } from './controllers/orders.controller';
 import { OrdersService } from './services/orders.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from './order.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([OrderEntity])
+    TypeOrmModule.forFeature([OrderEntity]),
+    CacheModule.register()
   ],
   controllers: [OrdersController],
   providers: [OrdersService]
